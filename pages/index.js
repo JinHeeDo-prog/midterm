940d9a0f1b95946e36551324e42ca60fbd5d353b
JinHeeDo-prog <gmleh03161@gmail.com>
2023-10-18 13:17:58 +0900

    Third Commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Link from "next/link";
+import Navbar from "@/components/NavBar";
+
 
 export default function Home() {
   return (
     <>
       <div>
           <p>pages/index.js 진희도 201930431 </p>
-          <Link href ="/">| Home</Link> |
-          <Link href ="/about"> About</Link> |
-          <Link href="/contact"> Contact</Link> |
+          <Navbar />
       </div>
     </>
   );
